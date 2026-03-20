7c69d0e560a6e87e531ee94d55f5ccc54429a066
Purna Nugraha <purnasatria@gmail.com>
2026-03-21 00:18:18 +0700

    use pre-built binary instead of building from source

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -6,62 +6,39 @@ echo "Activating feature 'neovim'"
 VERSION=${VERSION:-stable}
 echo "The version to be installed is: $VERSION"
 
-# Debian / Ubuntu dependencies
-install_debian_dependencies() {
-  apt-get update -y
-  apt-get -y install ninja-build gettext cmake unzip curl build-essential
-
-  apt-get -y clean
-  rm -rf /var/lib/apt/lists/*
-}
-
-# ******************
-# ** Main section **
-# ******************
-
-
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    echo 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
-ADJUSTED_VERSION=$VERSION
-
-if [  "$VERSION" != "stable" ] && [  "$VERSION" != "nightly" ]; then
-    ADJUSTED_VERSION="v$VERSION"
-fi
-
-# Bring in ID, ID_LIKE, VERSION_ID, VERSION_CODENAME
-. /etc/os-release
-# Get an adjusted ID independent of distro variants
-if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]; then
-    ADJUSTED_ID="debian"
-# other distros to be implemented
-# elif [[ "${ID}" = "rhel" || "${ID}" = "fedora" || "${ID}" = "mariner" || "${ID_LIKE}" = *"rhel"* || "${ID_LIKE}" = *"fedora"* || "${ID_LIKE}" = *"mariner"* ]]; then
-  # todo
-# elif [ "${ID}" = "alpine" ]; then
-  # todo
-else
-    echo "Linux distro ${ID} not supported."
-    exit 1
-fi
-
-# Install packages for appropriate OS
-case "${ADJUSTED_ID}" in
-    "debian")
-        install_debian_dependencies
+# Determine architecture
+ARCH=$(uname -m)
+case "${ARCH}" in
+    x86_64)  NVIM_ARCH="x86_64" ;;
+    aarch64) NVIM_ARCH="arm64" ;;
+    *)
+        echo "Unsupported architecture: ${ARCH}"
+        exit 1
         ;;
 esac
 
-echo "Downloading source for ${ADJUSTED_VERSION}..."
+TARBALL="nvim-linux-${NVIM_ARCH}.tar.gz"
 
-curl -sL https://github.com/neovim/neovim/archive/refs/tags/${ADJUSTED_VERSION}.tar.gz | tar -xzC /tmp 2>&1
+# Determine download URL
+if [ "$VERSION" = "stable" ]; then
+    DOWNLOAD_URL="https://github.com/neovim/neovim/releases/latest/download/${TARBALL}"
+elif [ "$VERSION" = "nightly" ]; then
+    DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/nightly/${TARBALL}"
+else
+    DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/v${VERSION}/${TARBALL}"
+fi
 
-echo "Building..."
+echo "Downloading neovim from ${DOWNLOAD_URL}..."
 
-cd /tmp/neovim-${ADJUSTED_VERSION}
-
-make CMAKE_BUILD_TYPE=Release && make CMAKE_INSTALL_PREFIX=/usr/local/nvim install
+curl -fLo "/tmp/${TARBALL}" "${DOWNLOAD_URL}"
+tar -C /usr/local -xzf "/tmp/${TARBALL}"
+mv "/usr/local/nvim-linux-${NVIM_ARCH}" /usr/local/nvim
 ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
+rm "/tmp/${TARBALL}"
 
-rm -rf /tmp/neovim-${ADJUSTED_VERSION}
+echo "Neovim $(nvim --version | head -1) installed successfully."
